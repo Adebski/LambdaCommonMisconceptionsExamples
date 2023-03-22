c57781fe911db3532de92c3881c880c64e69e14e
Andrzej Dębski <andrzej.debski91@gmail.com>
2023-03-22 15:39:04 +0100

    Update MultiplePathsHandlerProvisionedConcurrency.cs

--- handlers-net/src/lambda-example/MultiplePathsHandlerProvisionedConcurrency.cs
+++ handlers-net/src/lambda-example/MultiplePathsHandlerProvisionedConcurrency.cs
@@ -45,6 +45,8 @@ namespace lambdaExample
         {
             this.constructTimeStart = System.Environment.TickCount;
             LambdaLogger.Log($"{DateTime.Now.ToString($"yyyy/MM/dd HH:mm:ss.ff")}: pre-warming with {SAMPLE_ITERATIONS} iterations");
+            // Pre-warming the most common code paths in the function handler constructor to take advantage
+            // of the provisioned concurrency function lifecycle
             for (int i = 0; i < SAMPLE_ITERATIONS; ++i)
             {
                 var exampleInput = new Dictionary<string, string> { { "testNumber", i.ToString() } };
